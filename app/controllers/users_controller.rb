9cca86424f3034d9dd51170a085bb844f1005e9f
root <root@mehdi-HP-EliteBook-8530w.(none)>
2013-05-16 19:09:30 +0200

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,35 @@ class UsersController < ApplicationController
 public
   def compare
     @test = true
-    begin
+#    begin
       #fill in the Hash with the average/total results for the user 1
-      query1 = User.find(params[:id]).first
+      query1 = User.find(params[:id])
       
       @user1 = {}
+      puts "----------------------------------->>> {1}"
       @user1[:distance] = query1.trainings.sum('distance').round(2)
+      puts "----------------------------------->>> {2}"
       @user1[:calories] = query1.trainings.sum('calories').round(2)
+      puts "----------------------------------->>> {3}"
       @user1[:speed] = query1.trainings.average('speed').round(2)
+      puts "----------------------------------->>> {4}"
       @user1[:email] = query1.email
-      
+      puts "----------------------------------->>> {5}"
       #fill in the Hash with the average/total results for the user 2
-      query2 = User.find_by_email(params[:email]).first
+      query2 = User.find_by_email(params[:email])
+      puts "----------------------------------->>> {6}"
       @user2 = {}
       @user2[:distance] = query2.trainings.sum('distance').round(2)
+      puts "----------------------------------->>> {7}"
       @user2[:calories] = query2.trainings.sum('calories').round(2)
+      puts "----------------------------------->>> {8}"
       @user2[:speed] = query2.trainings.average('speed').round(2)
+      puts "----------------------------------->>> {9}"
       @user2[:email] = query2.email
-    rescue
-      @test = false
-    end
+      puts "----------------------------------->>> {10}"
+#    rescue
+#      @test = false
+#    end
     respond_to do |format|
       if @test == true
         format.html
